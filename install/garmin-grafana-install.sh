f390d3b504ddee9b3e0db4fb0b51008a85ad32a5
Aliaksei Pilko <a.pilko@soton.ac.uk>
2025-05-09 13:21:57 +0100

    Add uv project dirs

--- install/garmin-grafana-install.sh
+++ install/garmin-grafana-install.sh
@@ -136,7 +136,7 @@ EOF
 if [ -z "$(ls -A /opt/garmin-grafana/.garminconnect)" ]; then
   # Run the script once to prompt for credential
   msg_info "Creating Garmin credentials, this will timeout in 60 seconds"
-  timeout 60s uv run --env-file /opt/garmin-grafana/.env /opt/garmin-grafana/src/garmin_grafana/garmin_fetch.py
+  timeout 60s uv run --env-file /opt/garmin-grafana/.env --project /opt/garmin-grafana/ /opt/garmin-grafana/src/garmin_grafana/garmin_fetch.py
   # Check if there is anything in the token dir now
   if [ -z "$(ls -A /opt/garmin-grafana/.garminconnect)" ]; then
     msg_error "Failed to create a token"
@@ -156,7 +156,7 @@ Description=garmin-grafana Service
 After=network.target
 
 [Service]
-ExecStart=uv run /opt/garmin-grafana/src/garmin_grafana/garmin_fetch.py
+ExecStart=uv run --project /opt/garmin-grafana/ /opt/garmin-grafana/src/garmin_grafana/garmin_fetch.py
 Restart=always
 EnvironmentFile=/opt/garmin-grafana/.env
 
